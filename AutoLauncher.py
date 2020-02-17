74dd99d38f0673176751f89e4cf0b606de2534e7
Nao6165 <stripecattail@gmail.com>
2020-02-17 23:47:03 +0900

    ブラウザとフォルダの起動機能を追加

--- AutoLauncher.py
+++ AutoLauncher.py
@@ -1,2 +1,22 @@
 import subprocess
-subprocess.Popen(r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE')
+import webbrowser
+
+# reference:https://kazusa-pg.com/python-open-folder/
+def open_folder(path):
+    subprocess.run('explorer {}'.format(path))
+
+
+def main():
+    open_folder('C:')
+
+    subprocess.Popen(r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE')
+
+    url = "amazon.co.jp"
+    browser = webbrowser.get(r'"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %s')
+    browser.open(url)
+
+#
+# main
+#
+if __name__ == '__main__':
+        main()
