b02630d523324f0d10c9c380eeac7221d0d5131b
Nao6165 <stripecattail@gmail.com>
2020-02-18 00:03:49 +0900

    make function "open_chrome"

--- AutoLauncher.py
+++ AutoLauncher.py
@@ -5,15 +5,17 @@ import webbrowser
 def open_folder(path):
     subprocess.run('explorer {}'.format(path))
 
+def open_chrome(url):
+    browser = webbrowser.get(r'"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %s')
+    browser.open(url)
 
 def main():
     open_folder('C:')
 
     subprocess.Popen(r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE')
 
-    url = "amazon.co.jp"
-    browser = webbrowser.get(r'"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %s')
-    browser.open(url)
+    url = "Amazon.co.jp"
+    open_chrome(url)
 
 #
 # main
